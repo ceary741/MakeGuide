de8c9bc8849755d0d070915cded9b51b172e56fc
muxy <muxy2001@outlook.com>
2024-07-11 17:44:47 +0800

    demo2

---
+++ demo2/data_gen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 1 1 > data
+echo 2 4 >> data
+echo 3 9 >> data
+echo 4 16 >> data
+echo 5 25 >> data
+echo 6 36 >> data
---
+++ demo2/plot.plt
@@ -0,0 +1,3 @@
+set terminal svg
+set output 'pic.svg'
+plot 'data' with linespoints
